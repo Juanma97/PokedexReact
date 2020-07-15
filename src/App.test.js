9453c228f3625806b7cb9914ba9029dfec79db68
Juanma97 <juanma97@outlook.com>
2020-07-15 15:05:18 +0100

    Refactor

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import { render, screen } from '@testing-library/react';
 import AppToolbar from './AppToolbar';
 
 test('toolbar title should be Pokedex', () => {
-  render(<AppToolbar />)
-  expect(screen.getByText('Pokedex')).toBeInTheDocument();
+    render(<AppToolbar />)
+    expect(screen.getByText('Pokedex')).toBeInTheDocument();
 })
